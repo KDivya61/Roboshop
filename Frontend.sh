1761c55c602bdde7506c1c65a19ff81aad3a1ec9
KDivya61 <Divyak2857646@gmail.com>
2023-03-24 06:31:56 +0000

    commit

--- Frontend.sh
+++ Frontend.sh
@@ -1,11 +1,14 @@
 code_dir=$(pwd)
-yum install nginx -y 
-rm -rf /usr/share/nginx/html/* 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
-cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip
+log_file=/tmp/roboshop.log
+rm -f ${log_file}
 
-cp ${code_dir}/configs/Nginx-robo.conf /etc/nginx/default.d/roboshop.conf
+yum install nginx -y &>>${log_file}
+rm -rf /usr/share/nginx/html/* &>>${log_file}
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>${log_file} 
+cd /usr/share/nginx/html  &>>${log_file}
+unzip /tmp/frontend.zip  &>>${log_file}
 
-systemctl enable nginx 
-systemctl start nginx
+cp ${code_dir}/configs/Nginx-robo.conf /etc/nginx/default.d/roboshop.conf  &>>${log_file}
+
+systemctl enable nginx  &>>${log_file}
+systemctl start nginx  &>>${log_file}
