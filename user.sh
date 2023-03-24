8d624e46a923b9b40d807fbfa8e957ad2c475698
KDivya61 <Divyak2857646@gmail.com>
2023-03-24 15:41:26 +0000

    commit

--- user.sh
+++ user.sh
@@ -1,11 +1,11 @@
 source Common.sh
 
 print_head "configure NOde Js repo"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>${log_file}
 status_check $?
 
 print_head "install nodejs"
-yum install nodejs -y
+yum install nodejs -y  &>>${log_file}
 
 print_head "add roboshob user"
 id roboshob  &>>${log_file}
@@ -13,23 +13,29 @@ if [ $? -ne 0]; then
 useradd roboshop  &>>${log_file}
 fi
 status_check $?
+
 print_head "add applic directory"
 if [ ! -d /app]; then
 mkdir /app  &>>${log_file}
 fi
 status_check $? 
+
 print_head "download app content"
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip  &>>${log_file}
 cd /app  
 status_check $?
+
 print_head "extracting app content"
 unzip /tmp/user.zip  &>>${log_file}
 status_check $?
+
 print_head "install nodejs dependencies"
 npm install  &>>${log_file}
 status_check $?
+
 cp ${code_dir}/configs/user.service /etc/systemd/system/user.service  &>>${log_file}
 status_check $?
+
 print_head "reload file"
 systemctl daemon-reload &>>${log_file}
 status_check $?
