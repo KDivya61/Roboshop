2c98e2c79426b6d96d5d5386ab1fb57ee7457489
KDivya61 <Divyak2857646@gmail.com>
2023-03-26 16:35:19 +0000

    commit

---
+++ Mysql.sh
@@ -0,0 +1,26 @@
+source Common.sh
+mysql_root_password=$1
+
+if [ -z "${mysql_root_password}" ]; then 
+exit 1
+fi
+
+print_head "disabling mysql older version"
+dnf module disable mysql -y &>>${log_file}
+status_check $?
+
+print_head "install mysql server"
+yum install mysql-community-server -y &>>${log_file}
+status_check $?
+
+print_head "enable mysql"
+systemctl enable mysqld &>>${log_file}
+status_check $?
+
+print_head "start mysql"
+systemctl start mysqld  &>>${log_file}
+status_check $?
+
+print_head "set pwd"
+mysql_secure_installation --set-root-pass ${mysql_root_password} &>>${log_file}
+status_check $?
