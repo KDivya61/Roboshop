cdec04acf396d5e7b148ae5538ac64581e3af54b
KDivya61 <Divyak2857646@gmail.com>
2023-03-27 18:43:18 +0000

    c

--- Common.sh
+++ Common.sh
@@ -23,6 +23,7 @@ print_head "copy systemD service file"
 cp ${code_dir}/configs/${component}.service /etc/systemd/system/${component}.service  &>>${log_file}
 status_check $?
 
+sed -i -e "s/ROBOSHOP_USER_PASSWORD/"${roboshop_app_password}/" /etc/systemd/system/${component}.service &>>${log_file}
 print_head "reload file"
 systemctl daemon-reload  &>>${log_file}
 status_check $?
@@ -135,5 +136,22 @@ status_check $?
 
 
 Schema_setup
+systemd_setup
+}
+python(){
+    
+    print_head "install python"
+    yum install python36 gcc python3-devel -y &>>${log_file}
+status_check $?
+
+app_prereq_setup
+
+print_head "dowmload dependencies"
+pip3.6 install -r requirements.txt  &>>${log_file}
+status_check $?
+
+
+
+
 systemd_setup
 }
